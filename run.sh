f5720b3c5963b4b0bcd77a5fff41cc98c35286ad
culiiik <mr.pavel.novak@gmail.com>
2024-08-31 13:53:02 +0200

    u

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+cd /mnt/sharedstorage0/
 mv -f /var/www/medama_linux_amd64 /mnt/sharedstorage0/
-chmod +x /mnt/sharedstorage0/medama_linux_amd64
-/mnt/sharedstorage0/medama_linux_amd64 start
+chmod +x medama_linux_amd64
+medama_linux_amd64 start
