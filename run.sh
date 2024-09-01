9759809e2419dbeed3cc8d55c9f713c94bc74f4f
culiiik <mr.pavel.novak@gmail.com>
2024-09-01 11:57:26 +0200

    u

--- run.sh
+++ run.sh
@@ -3,4 +3,6 @@
 echo $PWD
 mv -f /var/www/medama_linux_amd64 /mnt/sharedstorage0/
 chmod +x /mnt/sharedstorage0/medama_linux_amd64
-/mnt/sharedstorage0/medama_linux_amd64 start
+cd /mnt/sharedstorage0/
+echo $PWD
+medama_linux_amd64 start
