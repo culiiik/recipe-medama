45f397c25044acb74f1abc747fe44ab5bf33179c
culiiik <mr.pavel.novak@gmail.com>
2024-09-01 12:25:39 +0200

    u

--- run.sh
+++ run.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
 
-echo $PWD
-mv -f /var/www/medama_linux_amd64 /mnt/sharedstorage0/
-chmod +x /mnt/sharedstorage0/medama_linux_amd64
-cd /mnt/sharedstorage0/
-echo $PWD
-medama_linux_amd64 start
+
+/var/www/medama_linux_amd64 start
